4a2114249259b513c5aaa4b9beeb440a7ec86cd2
danilodn7 <168425842+danilodn7@users.noreply.github.com>
2024-05-15 10:42:36 -0300

    Create pessoa.java

---
+++ pessoa.java
@@ -0,0 +1,19 @@
+public class Pessoa {
+	int CPF;
+	String nome;
+	String cor;
+	String etnia;
+	String sexo;
+	String genero;
+	double altura;
+	double peso;
+	String formatoOlho;
+	String corOlho;
+	String tamanhoCabelo;
+	String corCabelo;
+	String formatoSobrancelha;
+	double tamanhoMao;
+	double tamanhoUnhaMao;
+	int tamanhoPe;
+	double tamanhoUnhaPe;
+}
